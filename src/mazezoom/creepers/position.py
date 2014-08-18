8b8cf63052f170b41dd5eee46b7180804a997329
zoug <b.zougang@gmail.com>
2014-08-18 18:32:50 +0800

    修改代码样式

--- src/mazezoom/creepers/position.py
+++ src/mazezoom/creepers/position.py
@@ -7,26 +7,40 @@ class OyksoftPosition(PositionSpider):
     domain = "www.oyksoft.com"
     charset = 'gbk'
     search_url = "http://www.oyksoft.com/GoogleSearch.html?q=%s"
-    base_xpath = "//table[@class='gsc-table-result']/tbody/tr/td[@class='gsc-table-cell-snippet-close']"
-    title_xpath = "child::div[@class='gs-title gsc-table-cell-thumbnail gsc-thumbnail-left']/a[@class='gs-title']"
-    link_xpath = "child::div[@class='gs-title gsc-table-cell-thumbnail gsc-thumbnail-left']/a[@class='gs-title']/@href"
+    base_xpath = ("//table[@class='gsc-table-result']/tbody/"
+                  "tr/td[@class='gsc-table-cell-snippet-close']")
+    title_xpath = ("child::div[@class='gs-title gsc-table-cell-thumbnail"
+                   " gsc-thumbnail-left']/a[@class='gs-title']")
+    link_xpath = ("child::div[@class='gs-title gsc-table-cell-thumbnail"
+                  " gsc-thumbnail-left']/a[@class='gs-title']/@href")
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         #获取搜索结果title和链接
         items = etree.xpath(self.base_xpath)
-        return [(item.xpath(self.link_xpath)[0], item.xpath(self.title_xpath)[0].text_content()) for item in items]
+        for item in items:
+            link = item.xpath(self.link_xpath)[0]
+            title = item.xpath(self.title_xpath)[0].text_content()
+            results.append((link, title))
+        return results
 
 
 class GameDogPosition(PositionSpider):
     domain = "www.gamedog.cn"
-    search_url = "http://zhannei.gamedog.cn/cse/search?s=10392184185092281050&entry=1&q=%s"
+    search_url = ("http://zhannei.gamedog.cn/cse/search"
+                  "?s=10392184185092281050&entry=1&q=%s")
     xpath = "//a[@class='result-game-item-title-link']"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class Positon365(PositionSpider):
@@ -36,9 +50,14 @@ class Positon365(PositionSpider):
     xpath = "//div[@class='searchTopic']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class Mm10086Position(PositionSpider):
@@ -47,32 +66,49 @@ class Mm10086Position(PositionSpider):
     xpath = "//dd[@class='sr_searchListConTt']/h2/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class Hack50Positon(PositionSpider):
     domain = "www.hack50.com"
     charset = 'gbk'
-    search_url = "http://so.hack50.com/cse/search?s=1849264326530945843&ie=gbk&q=%s&m=2&x=20&y=12"
+    search_url = ("http://so.hack50.com/cse/search"
+                  "?s=1849264326530945843&ie=gbk&q=%s&m=2&x=20&y=12")
     xpath = "//h3[@class='c-title']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class Position520Apk(PositionSpider):
     domain = "www.520apk.com"
-    search_url = "http://search.520apk.com/cse/search?s=17910776473296434043&q=%s"
+    search_url = ("http://search.520apk.com/cse/search"
+                  "?s=17910776473296434043&q=%s")
     xpath = "//h3[@class='c-title']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class Apk3Position(PositionSpider):
@@ -81,9 +117,14 @@ class Apk3Position(PositionSpider):
     xpath = "//div[@class='searchTopic']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class DownzaPosition(PositionSpider):
@@ -93,21 +134,42 @@ class DownzaPosition(PositionSpider):
     xpath = "//div[@class='soft_list mb_20 clearfix']/dl/dd/h2/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class DreamsDownPosition(PositionSpider):
     domain = "www.dreamsdown.com"
     charset = 'gbk'
-    search_url = "http://search.discuz.qq.com/f/discuz?mod=curforum&formhash=096d51fb&srchtype=title&srhfid=170&srhlocality=forum%3A%3Aforumdisplay&sId=19235686&ts=1408356224&cuId=0&cuName=&gId=7&agId=0&egIds=&fmSign=&ugSign7=&sign=7e9565583d199fecff9503b381840530&charset=gbk&source=discuz&fId=170&q=%s&srchtxt=%s&searchsubmit=true"
-    xpath =  "//h3[@class='title']/a"
+    search_url = (
+        "http://search.discuz.qq.com/f/discuz"
+        "?mod=curforum&formhash=096d51fb"
+        "&srchtype=title&srhfid=170"
+        "&srhlocality=forum%3A%3Aforumdisplay&sId=19235686"
+        "&ts=1408356224&cuId=0&cuName="
+        "&gId=7&agId=0&egIds=&fmSign=&ugSign7="
+        "&sign=7e9565583d199fecff9503b381840530"
+        "&charset=gbk&source=discuz"
+        "&fId=170&q=%s&srchtxt=%s&searchsubmit=true"
+    )
+    xpath = "//h3[@class='title']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(item.attrb('href'), item.text_content()) for item in items]
+        items = etree.xpath(self.xpath)
+        for item in items:
+            link = item.attrib['href']
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 class AnZhiPosition(PositionSpider):
@@ -116,9 +178,14 @@ class AnZhiPosition(PositionSpider):
     xpath = "//span[@class='app_name']/a"
 
     def run(self, appname):
+        results = []
         etree = self.send_request(appname)
         items = etree.xpath(self.xpath)
-        return [(self.normalize_url(self.search_url, item.attrb('href')), item.text_content()) for item in items]
+        for item in items:
+            link = self.normalize_url(self.search_url, item.attrib('href'))
+            title = item.text_content()
+            results.append((link, title))
+        return results
 
 
 if __name__ == "__main__":
