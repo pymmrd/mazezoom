03d7842cc97a019cbc7cede5c4d69a5e1e3232e9
zoug <b.zougang@gmail.com>
2014-08-21 18:25:49 +0800

    oyksoftchannel

--- src/mazezoom/creepers/channel.py
+++ src/mazezoom/creepers/channel.py
@@ -8,9 +8,14 @@
 """
     渠道下载页面爬虫:
     抓取因子：软件大小，版本，下载次数, 软件
+    下载次数抓取策略：
+        1.位于detail页面的可以直接抓取
+        2.位于搜索结果中的可以保存此app在搜索结果中的名字,
+          重新搜索获取下载次数
 """
 
 from base import ChannelSpider
+from position import OyksoftPosition
 
 
 class OykSoftChannel(ChannelSpider):
@@ -24,12 +29,18 @@ class OykSoftChannel(ChannelSpider):
         运行环境：GoogleAndroid
         更新时间：2014/8/19 13:00:42
         关键词项：VST全聚合 全媒体聚合 VST电视直播 电视回播 视频点播
+    
     """
     domain = "www.oyksoft.com"
     down_xpath = "//a[@class='normal']/@href"
     fuzzy_xpath = "//div[@id='softinfo']/ul/li"
     label_xpath = "child::strong/text()"
     value_xpath = "child::text()"
+    search_cls = OyksoftPosition
+
+    def download_times(self, title): 
+        position = self.search_cls()
+        position.run(title, True)
 
     def run(self, url):
         result = {}
