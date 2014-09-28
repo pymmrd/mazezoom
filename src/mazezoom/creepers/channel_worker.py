545b725e593360a9bbe00e918d57f41287bbfbd9
zoug <b.zougang@gmail.com>
2014-09-28 12:33:37 +0800

    channel worker

--- src/mazezoom/creepers/channel_worker.py
+++ src/mazezoom/creepers/channel_worker.py
@@ -16,16 +16,42 @@ from backend import RedisBackend
 INTERUPT = 1
 
 
+def update_first_status(pk):
+    ChannelLink.objects.filter(pk=pk).update(is_first=False)
+
+
 def worker():
     backend = RedisBackend()
     while 1:
         task = backend.accept(CHANNEL_TASK_KEY)
         if task is not None:
-            appname, appid, clsname = task
-            cls = ChannelSpider.get(clsname, None)
+            loadtask = json.loads(task)
+            length = len(loadtask)
+            if length  == 7:
+                #crontab dispatcher
+                (channellink, app_uuid, app_version, url,
+                    channel, title, clsname) = loadtask 
+                    is_first = False
+            elif length == 8:
+                (channellink, app_uuid, app_version, url,
+                    channel, title, clsname, is_first) = loadtask 
+            cls = ChannelSpider.subclass.get(clsname, None)
             if cls is not None:
-                instance = cls()
-                instance.run()
+                instance = cls(
+                    channellink=channellink,
+                    app_uuid=app_uuid,
+                    app_version=app_version,
+                    url=url,
+                    channel=channel,
+                    title=title
+                )
+                try:
+                    instance.run()
+                except:
+                    pass
+                else:
+                    if is_first:
+                        update_first_status(channellink)
         time.sleep(INTERUPT)
 
 if __name__ == "__main__":
