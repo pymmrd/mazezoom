fcfda0332152300f41ad2befdcb2eb54d539a570
zoug <b.zougang@gmail.com>
2014-09-29 19:01:57 +0800

    channel

--- src/mazezoom/creepers/channel/channel_chenyu.py
+++ src/mazezoom/creepers/channel/channel_chenyu.py
@@ -26,42 +26,6 @@ if __name__ != '__main__':
 from base import ChannelSpider
 
 
-class HiapkChannel(ChannelSpider):
-    """
-    ***无下载次数***
-    url ： http://apk.hiapk.com/appinfo/com.tencent.mm
-    作者： 腾讯科技广州分公司
-    热度： 2.3亿热度
-    大小： 25.12MB
-    类别： 社交
-    语言： 中文
-    固件： 2.2及以上固件版本
-    支持屏幕： 适用分辨率
-    上架时间： 2014-07-17
-    """
-
-    domain = "apk.hiapk.com"
-    fuzzy_xpath = "//div[@class='code_box_border']/div[@class='line_content']"
-    lable_xpath = "child::span[1]/text()"
-    value_xpath = "child::span[2]/text()"
-    down_xpath = "//a[@class='link_btn']/@href"
-
-    def run(self, url):
-        result = {}
-        stroage = None
-        etree = self.send_request(url)
-        items = etree.xpath(self.fuzzy_xpath)
-        for item in items:
-            if item is not None:
-                label = item.xpath(self.lable_xpath)[0]
-                value = item.xpath(self.value_xpath)[-1]
-                result[label.strip()] = value.strip()
-
-        down_link = etree.xpath(self.down_xpath)[0]
-        down_link = self.normalize_url(url, down_link)
-        if down_link:
-           storage = self.download_app(down_link)
-        return result 
 
 class GfanChannel(ChannelSpider):
     """
