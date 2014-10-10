758feab1fa4728b5663bb5c83ac55ba07a2d183e
root <root@zg163-Lenovo-IdeaPad-Y470.(none)>
2014-10-10 23:17:41 +0800

    production

--- src/mazezoom/settings/production.py
+++ src/mazezoom/settings/production.py
@@ -7,21 +7,31 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'enginet',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'enginet',                      
         'USER': 'admin',
-        'PASSWORD': 'admin4Mazezoom',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PASSWORD': 'admin4u',
+        'HOST': '',                      
+        'PORT': '',                      
         'STORAGE_ENGINE': 'INNODB',
     }
 }
 
-MONGO_HOST = '10.9.19.178'
+MONGO_HOST = '127.0.0.1'
 MONGO_PORT = 27017
 MONGO_DB = 'mazezoom'
 MONGO_POOL_AUTH = {
     'user': 'root',
     'passwd': 'admin4u',
 }
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_PASSWORD = 'afc7c7180c3c43b51b1ebfebae76b5e8'
+REDIS_CONF = {
+    'host': REDIS_HOST,
+    'password': REDIS_PASSWORD,
+    'port': REDIS_PORT,
+    'db': 0,
+}
+
