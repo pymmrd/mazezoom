096eafc5632fc32c0d93f1220783d0ddec2f3812
chenyuxxgl <chenyuxxgl@126.com>
2014-10-13 01:11:32 +0800

    channel2.py

--- src/mazezoom/creepers/channel/channel2.py
+++ src/mazezoom/creepers/channel/channel2.py
@@ -54,23 +54,173 @@ class ZhuShou360Channel(ChannelSpider):
 
 class MiChannel(ChannelSpider):
     """
-    无下载次数
+    url: http://app.mi.com/detail/19732
+    软件大小: 30.82 M
+    版本号：1.9.6
+    更新时间：2014-09-01
+    包名：com.halfbrick.fruitninja
     """
     domain = "app.mi.com"
-    fuzzy_xpath = "//div[@class='details preventDefault']/ul/li/text()"
+    times_xpath = "//div[@class='intro-titles']/span[@class='app-intro-comment']"
+    fuzzy_xpath = "//div[@class='details preventDefault']/ul/li[@class='weight-font']"
+    value_xpath = "following::li"
 
+    def download_times(self, etree):
+        times = None
+
+        regx = re.compile('\d+')
+        dom = etree.xpath(self.times_xpath)[0]
+        number_text = dom.text_content().strip()
+        if number_text:
+            match = regx.search(number_text)
+            if match is not None:
+                rawtimes = match.group(0)
+                try:
+                    times = int(rawtimes)
+                except (TypeError, ValueError):
+                    pass
+        return times
+
+    def parser(self):
+        result = {}
+        mapping = {
+            u'软件大小': 'size',
+            u'版本号': 'human_version',
+            u'更新时间': 'update_time',
+            u'包名': 'name',
+        }
+
+        etree = self.send_request(self.url)
+        items = etree.xpath(self.fuzzy_xpath)
+        for item in items:
+            label = item.text_content().strip()[:-1]
+            value_dom = item.xpath(self.value_xpath)[0]
+            value = value_dom.text_content()
+            label = mapping.get(label, '')
+            if label:
+                result[label] = value.strip()
+
+        times = self.download_times(etree)
+        result['download_times'] = times
+        #down_link = self.download_link(etree)
+        #if down_link:
+            #storage = self.download_app(down_link)
+        print result
+        return result
 
 class NearMeChannel(ChannelSpider):
+    """
+    url: http://store.nearme.com.cn/product/0000/563/963_1.html?from=1152_2
+    发布时间：2014.09.28
+    大小：29.51M
+    版本：1.9.6
+    类别：休闲游戏
+    适用系统：Andriod2.3.3及以上
+    下载：900万+次下载 
+    """
+    seperator = u'：'
     domain = "store.nearme.com.cn"
+    times_xpath = "//div[@class='soft_info_nums']/text()"
+    fuzzy_xpath = "//ul[@class='soft_info_more']/li"
+
+    def download_times(self, etree):
+        times = None
+
+        translate = u"万"
+        regx = re.compile('\d+')
+        number_text = ''.join(etree.xpath(self.times_xpath)).strip()
+        if number_text:
+            match = regx.search(number_text)
+            if match is not None:
+                rawtimes = match.group(0)
+                try:
+                    times = int(rawtimes)
+                except (TypeError, ValueError):
+                    pass
+                else:
+                    if translate in number_text:
+                        times = times * 10000
+        return times
+
+    def parser(self):
+        result = {}
+        mapping = {
+            u'大小': 'size',
+            u'版本': 'human_version',
+            u'发布时间': 'update_time',
+            u'类别': 'category',
+            u'适用系统': 'env',
+        }
+
+        etree = self.send_request(self.url)
+        items = etree.xpath(self.fuzzy_xpath)
+        for item in items:
+            content = item.text_content().strip()
+            elems = content.split(self.seperator)
+            if len(elems) == 2:
+                label, value = elems
+                label = label.strip()
+                label = mapping.get(label, '')
+                if label:
+                    result[label] = value.strip()
+                    print label, value
+
+        times = self.download_times(etree)
+        result['download_times'] = times
+        #down_link = self.download_link(etree)
+        #if down_link:
+            #storage = self.download_app(down_link)
+        print result
+        return result
+
+class UCChannel(ChannelSpider):
+    """
+    url: http://apps.uc.cn/detail?appChannelId=456916
+    版本：V1.0.0
+    分类：动作游戏
+    大小：8.7M
+    更新时间：2014-10-10
+    资费说明：使用完全免费
+    """
+    seperator = u'：'
+    domain = "apps.uc.cn"
+    fuzzy_xpath = "//div[@class='param']/dl[position()<last()]"
+
+    def parser(self):
+        result = {}
+        mapping = {
+            u'大小': 'size',
+            u'版本': 'human_version',
+            u'更新时间': 'update_time',
+            u'分类': 'category',
+            u'资费说明': 'price',
+        }
+
+        etree = self.send_request(self.url)
+        items = etree.xpath(self.fuzzy_xpath)
+        for item in items:
+            content = item.text_content().strip()
+            print content
+            elems = content.split(self.seperator)
+            if len(elems) == 2:
+                label, value = elems
+                label = label.strip()
+                label = mapping.get(label, '')
+                if label:
+                    result[label] = value.strip()
+                    print label, value
+
+        #times = self.download_times(etree)
+        #result['download_times'] = times
+        #down_link = self.download_link(etree)
+        #if down_link:
+            #storage = self.download_app(down_link)
+        print result
+        return result
 
 
 class LenovoChannel(ChannelSpider):
     domain = 'app.lenovo.com'
-
-
-class UCChannel(ChannelSpider):
-    domain = "apps.uc.cn"
-
 
 class M163Channel(ChannelSpider):
     domain = "m.163.com"
@@ -165,3 +315,34 @@ class LeidianChannel(ChannelSpider):
 
 class Channel2265(ChannelSpider):
     domain = "www.2265.com"
+
+if __name__ == '__main__':
+    mi = MiChannel(
+        channellink=1,
+        app_uuid='1',
+        app_version=1,
+        channel=1,
+        url='http://app.mi.com/detail/19732',
+        title=u'水果忍者'
+    )
+    #mi.run()
+
+    nearme = NearMeChannel(
+        channellink=1,
+        app_uuid='1',
+        app_version=1,
+        channel=1,
+        url='http://store.nearme.com.cn/product/0000/553/613_1.html?from=1152_1',
+        title=u'水果忍者'
+    )
+    #nearme.run()
+
+    uc = UCChannel(
+        channellink=1,
+        app_uuid='1',
+        app_version=1,
+        channel=1,
+        url='http://apps.uc.cn/detail?appChannelId=456916',
+        title=u'忍者砍僵尸'
+    )
+    #uc.run()
